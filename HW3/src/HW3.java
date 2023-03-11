86e1ef59daae88018c7d47ffdb4218491ca19d1a
VladimirS0l <vbnm1993@yandex.ru>
2023-03-11 13:54:00 +0300

    HW3

---
+++ HW3/src/HW3.java
@@ -0,0 +1,70 @@
+public class HW3 {
+    Node head;
+    Node tail;
+
+    public void revert(){
+        if (head != null && head.next != null){
+            Node temp = head;
+            revert(head.next, head);
+            temp.next = null;
+        }
+    }
+
+    private void revert(Node currentNode, Node previousNode){
+        if (currentNode.next == null){
+            head = currentNode;
+        }else {
+            revert(currentNode.next, currentNode);
+        }
+        currentNode.next = previousNode;
+        previousNode.next = null;
+    }
+
+    public void add(int value, Node node){
+        Node next = node.next;
+        Node newNode = new Node();
+        newNode.value = value;
+        node.next = newNode;
+        newNode.previous = node;
+        if (next == null){
+            tail = newNode;
+        }else {
+            next.previous = newNode;
+            newNode.next = next;
+        }
+    }
+
+    public void delete(Node node){
+        Node previous = node.previous;
+        Node next = node.next;
+        if (previous == null){
+            next.previous = null;
+            head = next;
+        }else {
+            if (next == null){
+                previous.next = null;
+                tail = previous;
+            }else {
+                previous.next = next;
+                next.previous = previous;
+            }
+        }
+    }
+
+    public Node find(int value) {
+        Node currentNode = head;
+        while (currentNode != null) {
+            if (currentNode.value == value) {
+                return currentNode;
+            }
+            currentNode = currentNode.next;
+        }
+        return null;
+    }
+
+    public class Node{
+        int value;
+        Node next;
+        Node previous;
+    }
+}
